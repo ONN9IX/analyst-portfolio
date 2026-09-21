fb47e3f2dc94793d2fb13a6cf8aaea9b638be6a0
ONN9IX <makssimmov548@gmail.com>
2026-09-21 12:59:38 +0300

    QA: use discounted Total_Sales in SQL

--- 03_sql_analysis/sql/01_basic_analysis.sql
+++ 03_sql_analysis/sql/01_basic_analysis.sql
@@ -2,6 +2,6 @@ SELECT
     COUNT(DISTINCT order_id) AS orders,
     COUNT(DISTINCT customer_name) AS customers,
     SUM(quantity) AS units,
-    ROUND(SUM(quantity * unit_price), 2) AS revenue,
-    ROUND(AVG(quantity * unit_price), 2) AS average_order_value
+    ROUND(SUM(total_sales), 2) AS revenue,
+    ROUND(AVG(total_sales), 2) AS average_order_value
 FROM ecommerce_sales;
