675ed0a17b291157b895cd78f1e25d05213e5f76
ONN9IX <makssimmov548@gmail.com>
2026-09-21 12:59:40 +0300

    QA: use discounted Total_Sales in SQL

--- 03_sql_analysis/sql/02_sales_analysis.sql
+++ 03_sql_analysis/sql/02_sales_analysis.sql
@@ -1,7 +1,7 @@
 SELECT
     DATE_TRUNC('month', order_date)::date AS month,
     COUNT(DISTINCT order_id) AS orders,
-    ROUND(SUM(quantity * unit_price), 2) AS revenue
+    ROUND(SUM(total_sales), 2) AS revenue
 FROM ecommerce_sales
 GROUP BY 1
 ORDER BY 1;
